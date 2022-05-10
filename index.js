3fba8df83811655955eabda31c89187ceb0a69b9
tomhughes87 <tomhughes1987@gmail.com>
2022-05-11 00:17:08 +0200

    add app.listen to port 3000

--- index.js
+++ index.js
@@ -11,4 +11,4 @@ app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
-app.listen(3000, () => console.log("Server is live!"));
+app.listen(3000, () => console.log("Server is live on port 3000!"));
