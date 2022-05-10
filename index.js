50f6de1ca697b162551e3e9557c00b87be2262c6
tomhughes87 <tomhughes1987@gmail.com>
2022-05-11 00:49:20 +0200

    add a app.get that posts data

--- index.js
+++ index.js
@@ -24,6 +24,12 @@ app.get("/pokemon/:id", (req, res) => {
 });
 // using ':' followed by text creates a var that can be used in req.params+...var
 
+//I think this is a push!
+app.get("/pokemon/post/:yourtext", (req, res) => {
+  res.send(req.params.yourtext);
+});
+// simple post the users :yourtext value to the page
+
 app.listen(3000, () => console.log("Server is live on port 3000!"));
 //test this with "node index.js"
 //test with nodemon index.js for live updates!
