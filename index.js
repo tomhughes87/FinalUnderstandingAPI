fae9a5f87631ab709c3d715a2d9a64fb555aa5e9
tomhughes87 <tomhughes1987@gmail.com>
2022-05-11 00:24:32 +0200

    add new app.get with '/pokemon'

--- index.js
+++ index.js
@@ -10,6 +10,12 @@ const app = express();
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
+//the "/" is just for the end of the url
+
+app.get("/pokemon", (req, res) => {
+  res.send([1, 2, 3]);
+});
+//the "/pokemon" mean that if you go to the url + pokemon you'll be sent the array
 
 app.listen(3000, () => console.log("Server is live on port 3000!"));
 //test this with "node index.js"
