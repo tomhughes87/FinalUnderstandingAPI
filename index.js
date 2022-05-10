78c407605784d6b28451c51d22a9fab72daadc4f
tomhughes87 <tomhughes1987@gmail.com>
2022-05-11 00:27:32 +0200

    add additional instructional comments and change array to pokemon

--- index.js
+++ index.js
@@ -13,9 +13,11 @@ app.get("/", (req, res) => {
 //the "/" is just for the end of the url
 
 app.get("/pokemon", (req, res) => {
-  res.send([1, 2, 3]);
+  res.send(["charmander", "bulbasaur", "squirtle"]);
 });
 //the "/pokemon" mean that if you go to the url + pokemon you'll be sent the array
 
 app.listen(3000, () => console.log("Server is live on port 3000!"));
 //test this with "node index.js"
+//test with nodemon index.js for live updates!
+//press ctrl + c to kill the terminal
